d8e917778bba2f46af249e10585fa98138b3384e
UnderKoen <koenvanstaveren@hotmail.com>
2025-02-27 16:47:34 +0100

    feat: allow for async configs

--- src/ConfigLoader.ts
+++ src/ConfigLoader.ts
@@ -2,7 +2,7 @@ import { ExtendConfig, TConfig } from "./types";
 import minimist from "minimist";
 import { defu } from "defu";
 
-type AdvanceConfig = (...args: unknown[]) => TConfig;
+type AdvanceConfig = (...args: unknown[]) => TConfig | Promise<TConfig>;
 
 export const DEFAULT_CONFIG: Readonly<TConfig> = Object.freeze({
   scripts: {},
@@ -17,18 +17,18 @@ export class ConfigLoader {
     return config;
   }
 
-  static loadExtensions(config: TConfig): TConfig[] {
+  static async loadExtensions(config: TConfig): Promise<TConfig[]> {
     if (!Object.hasOwn(config, "extends") || !config.extends) return [];
 
-    return [...config.extends]
-      .map((c) => this.loadFile(c, false))
-      .filter((c): c is TConfig => c !== undefined);
+    return (
+      await Promise.all([...config.extends].map((c) => this.loadFile(c, false)))
+    ).filter((c): c is TConfig => c !== undefined);
   }
 
-  static loadFile(
+  static async loadFile(
     file: ExtendConfig | undefined,
     noBail = true,
-  ): TConfig | undefined {
+  ): Promise<TConfig | undefined> {
     if (!file) return undefined;
     let source = this.getSource(file);
 
@@ -40,10 +40,10 @@ export class ConfigLoader {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       let config = require(source) as TConfig | AdvanceConfig;
       if (typeof config === "function") {
-        config = config(...(Array.isArray(file) ? file.slice(1) : []));
+        config = await config(...(Array.isArray(file) ? file.slice(1) : []));
       }
 
-      const configs = this.loadExtensions(config);
+      const configs = await this.loadExtensions(config);
 
       return defu(config, ...configs, DEFAULT_CONFIG);
     } catch (e) {
@@ -74,7 +74,7 @@ export class ConfigLoader {
     }
   }
 
-  static load(argv: minimist.ParsedArgs): TConfig {
+  static async load(argv: minimist.ParsedArgs): Promise<TConfig> {
     const possibleConfigFiles: string[] = [
       argv.config,
       process.env.BSM_CONFIG,
@@ -83,7 +83,7 @@ export class ConfigLoader {
     ].filter((s): s is string => s !== undefined);
 
     for (const file of possibleConfigFiles) {
-      const config = this.loadFile(file);
+      const config = await this.loadFile(file);
       if (!config) continue;
 
       process.env.BSM_CONFIG = file;
