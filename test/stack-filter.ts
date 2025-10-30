6a59d989990960aabdb0b24e4b9d9009e2099efa
UnderKoen <koenvanstaveren@hotmail.com>
2025-10-30 13:03:08 +0100

    chore: fix deploy script

--- test/stack-filter.ts
+++ test/stack-filter.ts
@@ -4,9 +4,10 @@ Error.prepareStackTrace = (err, stackTraces) => {
   // Filter out anything that comes from uvu itself
   const filtered = stackTraces.filter((callSite) => {
     const fileName = callSite.getFileName() || "";
-    return !fileName.match(/node_modules[\/\\]uvu/g);
+    return !fileName.match(/node_modules[/\\]uvu/g);
   });
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return originalPrepareStackTrace
     ? originalPrepareStackTrace(err, filtered)
     : `${err}\n${filtered.join("\n")}`;
